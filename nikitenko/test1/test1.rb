b96525499e87cd7f9577206a24a0792299fda43c
darya1996 <dnikitenko1996@gmail.com>
2019-04-06 16:24:29 -0400

    Test

---
+++ nikitenko/test1/test1.rb
@@ -0,0 +1,22 @@
+require_relative 'accounts'
+
+class Web_Banking< Accounts
+  def run 
+    browser.goto('https://my.fibank.bg/oauth2-server/login?client_id=E_BANK')
+	  browser
+	  go_to_demo
+	  get_account_info
+  end	
+
+  def go_to_demo
+	  browser.a(id: "demo-link").click
+  end	
+
+  def browser
+	  @browser ||= Watir::Browser.new :chrome
+  end	
+end	
+
+
+wb = Web_Banking.new
+wb.run
---
+++ nikitenko/test1/transactions.rb
@@ -0,0 +1,26 @@
+require 'nokogiri'
+require 'watir'
+require 'pry'
+require 'open-uri'
+
+class Transactions
+  def get_transaction_info
+	@transactions = {}.to_a
+	for i in 0..4
+	  @transactions << transaction_information(i)
+	end
+	puts JSON.pretty_generate @transactions
+  end	
+
+  def transaction_information(i)
+	{
+	  "date" => transaction_html.css("tbody tr")[i].css("td[class=ng-scope] span")[0].text,
+	  "description" => transaction_html.css("tbody tr")[i].css("td")[2].css("p")[0].text,
+	  "amount" => transaction_html.css("tbody tr")[i].css("td[class=ng-scope] span")[3].text
+	}
+  end
+
+  def transaction_html
+	Nokogiri::HTML.fragment(browser.table(id: "lastFiveTransactions").html)
+  end	
+end	
