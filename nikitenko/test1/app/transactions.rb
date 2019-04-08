0316052a5102e1d7d89da6258800457b87229ea8
Daria Nikitenko <38050647+darya1996@users.noreply.github.com>
2019-04-08 09:37:05 +0300

    Update transactions.rb

--- nikitenko/test1/app/transactions.rb
+++ nikitenko/test1/app/transactions.rb
@@ -5,23 +5,23 @@ require 'open-uri'
 
 class Transactions
   def get_transaction_info
-	@transactions = {}.to_a
-	for i in 0..4
-	  @transactions << transaction_information(i)
-	end
-	puts JSON.pretty_generate @transactions
+    @transactions = {}.to_a
+    for i in 0..4
+      @transactions << transaction_information(i)
+    end
+    puts JSON.pretty_generate @transactions
   end	
 
   def transaction_information(i)
-	{
-	  "date" => transaction_html.css("tbody tr")[i].css("td[class=ng-scope] span")[0].text,
-	  "description" => transaction_html.css("tbody tr")[i].css("td")[2].css("p")[0].text,
-	  "amount" => transaction_html.css("tbody tr")[i].css("td[class=ng-scope] span")[3].text
-	}
+    {
+      "date" => transaction_html.css("tbody tr")[i].css("td[class=ng-scope] span")[0].text,
+      "description" => transaction_html.css("tbody tr")[i].css("td")[2].css("p")[0].text,
+      "amount" => transaction_html.css("tbody tr")[i].css("td[class=ng-scope] span")[3].text
+    }
   end
 
   def transaction_html
-	Nokogiri::HTML.fragment(browser.table(id: "lastFiveTransactions").html)
+    Nokogiri::HTML.fragment(browser.table(id: "lastFiveTransactions").html)
   end	
 
 end	
