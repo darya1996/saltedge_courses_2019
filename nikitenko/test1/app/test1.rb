e66f0ff479dfb7a797c81523734a952c33cf4d7b
Daria Nikitenko <38050647+darya1996@users.noreply.github.com>
2019-04-08 09:35:01 +0300

    Update test1.rb

--- nikitenko/test1/app/test1.rb
+++ nikitenko/test1/app/test1.rb
@@ -3,16 +3,16 @@ require_relative 'accounts'
 class Web_Banking< Accounts
   def run 
     browser.goto('https://my.fibank.bg/oauth2-server/login?client_id=E_BANK')
-	  browser
-	  go_to_demo
-	  get_account_info
+    browser
+    go_to_demo
+    get_account_info
   end	
 
   def go_to_demo
-	  browser.a(id: "demo-link").click
+    browser.a(id: "demo-link").click
   end	
 
   def browser
-	  @browser ||= Watir::Browser.new :chrome
+    @browser ||= Watir::Browser.new :chrome
   end	
 end	
