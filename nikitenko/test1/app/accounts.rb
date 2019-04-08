c7f2b025feb97bdb31ae0342e029244caed1df68
Daria Nikitenko <38050647+darya1996@users.noreply.github.com>
2019-04-08 09:36:13 +0300

    Update accounts.rb

--- nikitenko/test1/app/accounts.rb
+++ nikitenko/test1/app/accounts.rb
@@ -3,25 +3,25 @@ require_relative 'transactions'
 class Accounts < Transactions
   def get_account_info
     @acc_hash = {}.to_a
-	@acc_hash << account_information
-	puts JSON.pretty_generate @acc_hash
-	get_transaction_info
+    @acc_hash << account_information
+    puts JSON.pretty_generate @acc_hash
+    get_transaction_info
   end	
 
   def account_information
-	{
-	  "name" => acc_name_html.at_css("h5#user--identificator").text,
-	  "balance" => card_html.css("tbody td[class=ng-scope] span")[1].text,
-	  "currency" => card_html.css("tbody td[class=ng-scope] span")[0].text,
-	}
+    {
+      "name" => acc_name_html.at_css("h5#user--identificator").text,
+      "balance" => card_html.css("tbody td[class=ng-scope] span")[1].text,
+      "currency" => card_html.css("tbody td[class=ng-scope] span")[0].text,
+    }
   end	
 
   def acc_name_html
-	Nokogiri::HTML.fragment(browser.div(class: "well").html)
+    Nokogiri::HTML.fragment(browser.div(class: "well").html)
   end	
 
   def card_html
-	Nokogiri::HTML.fragment(browser.table(id: "dashboardDeposits").html)
+    Nokogiri::HTML.fragment(browser.table(id: "dashboardDeposits").html)
   end	
 
 end	
